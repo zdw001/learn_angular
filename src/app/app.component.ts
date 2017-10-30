184bf79d65a07f9fe2b7b271e16737d7ff2f420e
Zach Winters <zwintersdeveloper@gmail.com>
2017-10-30 15:50:00 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>{{ title }}</h1><p>{{ description }} is cool</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hello srvup!';
+  description = 'a new app'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoListComponent,
+    VideoDetailComponent
   ],
   imports: [
     BrowserModule
