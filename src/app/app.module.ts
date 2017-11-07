d827c332ba77ab899344fb03b6c8c49b556052aa
Zach Winters <zwintersdeveloper@gmail.com>
2017-11-07 15:28:14 -0500

    6/28 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 
@@ -12,7 +15,10 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
     VideoDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
+	title = "Video List";
+	// videoList = ["item 1", "item 2", "item 3"];
+	videoList = [
+		{
+			name: "Item 1",
+			slug: "item-1"
+		},
+		{
+			name: "Item 2",
+			slug: "item-2"
+		},
+		{
+			name: "Item 3",
+			slug: "item-3"
+		},
 
+	]
   constructor() { }
 
   ngOnInit() {
